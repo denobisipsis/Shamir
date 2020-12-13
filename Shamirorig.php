c18ba6a8d61ba988736fb9afb0b8cccea38dbe30
denobisipsis <denobisipsis@users.noreply.github.com>
2020-12-13 11:48:37 +0100

    Update Shamirorig.php

--- Shamirorig.php
+++ Shamirorig.php
@@ -227,7 +227,7 @@ function split($secret,$t,$n)
 	
 	$this->degree = strlen(ltrim(bin2hex($secret),"0"))*4;
 	
-	if (($this->degree < 8) or ($this->degree > 1024) ) die("Max 128 caracteres, Min 64");
+	if (($this->degree < 64) or ($this->degree > 1024) ) die("Max 128 caracteres, Min 8");
 	
 	$secret = gmp_import($secret);			
 	      
